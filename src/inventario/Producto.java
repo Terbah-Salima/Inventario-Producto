b80af1d3f41f6b637d4515f9dd995e288e18c5b9
pc <pc@SALMA>
2025-02-18 20:14:16 +0100

    Action 1: Mover clases al paquete inventario

---
+++ src/inventario/Producto.java
@@ -0,0 +1,31 @@
+package inventario;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ * @terbah salima
+ */
+public class Producto {
+    public String n;
+    public double p;
+    public int s;
+
+    public Producto(String nombre, double precio, int stock) {
+        this.n = nombre;
+        this.p = precio;
+        this.s = stock;
+    }
+
+    public void mostrarInformacion() {
+        System.out.println("Producto: " + n + " | Precio: " + p + " | Stock: " + s);
+    }
+
+    public void borrarDatos() {
+        n = "";
+        p = 0.0;
+        s = 0;
+    }
+}
